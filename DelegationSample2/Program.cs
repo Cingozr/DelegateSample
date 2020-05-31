710dba3a17dff53c11246e02e1589cc36396d127
Recai Cingöz <recai@rise-consulting.net>
2020-05-31 13:47:49 +0300

    Add project files.

---
+++ DelegationSample2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DelegationSample2
+{
+    class Program
+    {
+        delegate int MathOperation(int x, int y);
+        static void Main(string[] args)
+        {
+            MathOperation mathOp = SimpleMath.Add;
+            mathOp += SimpleMath.Subtract;
+
+            foreach (Delegate del in mathOp.GetInvocationList())
+            {
+                Console.WriteLine($"Method Name: {del.Method}");
+                Console.WriteLine($"Method Name: {del.Target}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DelegationSample2/SimpleMath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegationSample2
+{
+    public class SimpleMath
+    {
+        public static int Add(int x, int y)
+        {
+            int retVal = x + y;
+            Console.WriteLine($"Add:{retVal}");
+            return retVal;
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            int retval = x - y;
+            Console.WriteLine($"Substract: {retval}");
+            return retval;
+        }
+    }
+}
